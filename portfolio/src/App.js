26940dfaaf54bf43609a9e4635a72200be8b13cf
unknown <nandhinimadan20@gmail.com>
2021-07-30 00:27:56 -0700

    Created basic 4 components {Home, AboutMe, ContactMe, Myprojects}

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav} from "react-bootstrap";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      
+    >
+      <Nav.Link href="#action1">Home</Nav.Link>
+      <Nav.Link href="#action2">About Me</Nav.Link>
+      <Nav.Link href="#action2">My Projects</Nav.Link>
+      <Nav.Link href="#action2">Contact Me</Nav.Link>
+
+      
+    </Nav>
+    
+  </Navbar.Collapse>
+</Navbar>
+<Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/AboutMe" component={AboutMe} />
+        <Route exact path="/MyProject" component={Myproject} />
+        <Route exact path="/ContactMe" component={ContactMe}/>
+      </Switch>
     </div>
   );
 }
