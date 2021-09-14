cb9d463424d68cea06efe59831c5cae3677ec56b
unknown <nandhinimadan20@gmail.com>
2021-09-14 11:40:00 -0700

    Added Skills in my portfolio

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col, Card,Button } from "react-bootstrap";
 import MerchantMarketPlace from "../src/asserts/MerchantMarket.png";
 import Snow from "../src/Snow";
 import SilentAuction from "../src/asserts/silentAuction.jpg";
@@ -40,18 +40,153 @@ const Home = () => {
         }
     ]
 
+
+    const skills=[{
+        skillId:1,
+        SkillName:"react",
+        link:""
+    },
+    {
+        skillId:2,
+        SkillName:"redux",
+        link:""
+    },
+    {
+        skillId:3,
+        SkillName:"Node",
+        link:""
+    },
+    {
+        skillId:4,
+        SkillName:"Express",
+        link:""
+    },
+    {
+        skillId:5,
+        SkillName:"JavaScript",
+        link:""
+    },
+    {
+        skillId:6,
+        SkillName:"HTML",
+        link:""
+    },
+    {
+        skillId:7,
+        SkillName:"CSS",
+        link:""
+    },
+    {
+        skillId:8,
+        SkillName:"Responsive Design",
+        link:""
+    },
+    {
+        skillId:9,
+        SkillName:"Context API",
+        link:""
+    },
+    {
+        skillId:10,
+        SkillName:"SQL",
+        link:""
+    },
+    {
+        skillId:11,
+        SkillName:"Knex",
+        link:""
+    },
+    {
+        skillId:12,
+        SkillName:"Axios",
+        link:""
+    },
+    {
+        skillId:13,
+        SkillName:"Yup",
+        link:""
+    },
+    {
+        skillId:14,
+        SkillName:"Boostrap",
+        link:""
+    },
+    {
+        skillId:15,
+        SkillName:"Styled Components",
+        link:""
+    },
+    {
+        skillId:16,
+        SkillName:"Client-Side Routing",
+        link:""
+    },
+    {
+        skillId:17,
+        SkillName:"JSON Web Tokens",
+        link:""
+    },
+    {
+        skillId:18,
+        SkillName:"Relational Database Design",
+        link:""
+    },
+    {
+        skillId:19,
+        SkillName:"Postman",
+        link:""
+    },
+    {
+        skillId:20,
+        SkillName:"Insomnia",
+        link:""
+    },
+    {
+        skillId:21,
+        SkillName:"Docker",
+        link:""
+    },
+    {
+        skillId:22,
+        SkillName:"Python",
+        link:""
+    },
+    {
+        skillId:23,
+        SkillName:"Material UI",
+        link:""
+    },
+    {
+        skillId:24,
+        SkillName:"PostgreSQL",
+        link:""
+    }
+    ]
+
     return (
         <>
             <Snow />
-
-            <h1>AboutMe</h1>
+            
+            <h2>AboutMe</h2>
             <br></br>
             <p>I'm a full-stack developer passionate about building software users love and that solve big problems at scale. I have experience building reusable frontend components using React, managing complex application state using Redux, building backend services using Node.js and Express.js, and database management using PostgreSQL.</p>
-
             <p> My background in learning and remote collaboration gives me the resources to ask the right questions, get the right answers, and delivering on time. And now I can build full-stack web applications I'm a natural fit for a remote software development team.</p>
-
              <p>I am currently seeking full-stack or frontend development opportunities based in Seattle, WA or anywhere in the world if remote working is an option.</p>
             <br></br>
+            <h2>Skills</h2>
+            <br></br>
+            <Row xs={12} md={12} className="g-4">
+                {skills.map((skill) => (
+                    <Col key={skill.skillId}>
+                        <Button href={skill.link}>{skill.SkillName}</Button> 
+                        
+                    </Col>
+                ))}
+            </Row>
+            <br></br>
+            <h2>My Projects</h2>
+            <br></br>
+            
             <Row xs={1} md={2} className="g-4">
                 {projectDetails.map((project) => (
                     <Col key={project.projectName}>
@@ -68,9 +203,12 @@ const Home = () => {
                                 </div>
                             </Card.Body>
                         </Card>
+                        <br></br>
                     </Col>
                 ))}
             </Row>
+
+
         </>
     )
 }
